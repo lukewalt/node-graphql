36052dbf21ae243965fb20346471740f2abc6dc1
Luke <luke.woodward@parallontechnology.com>
2018-04-05 16:17:08 -0500

    created link type and added that as selection set of query field

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////////
+// - GraphQL root types: Query Mutation Subscription correspond to operations
+// - Within these root types are fields and selection sets that define the querys/muts
+///////////////////////////////////////////////////////////////////////////////////
+
 const { GraphQLServer } = require('graphql-yoga');
 
-// GraphQL root types: Query Mutation Subscription
 const typeDefs = `
 type Query {
 	info: String!
-}`
+	feed: [Link!]!
+}
+
+type Link {
+	id: ID!
+	description: String!
+	url: String!
+}
+`
+
 
 const resolvers = {
 	Query: {
 		info: () => `This is a GraphQL API`
 	}
 }
+
+
 
 const server = new GraphQLServer({
 	typeDefs,
