ccfc7528b8d84a7bd5945199cd04f84fb0fc9b17
Luke <luke.woodward@parallontechnology.com>
2018-04-06 10:16:47 -0500

    refactored update link method and created delete method in resolver function

--- src/index.js
+++ src/index.js
@@ -6,31 +6,57 @@
 const { GraphQLServer } = require('graphql-yoga');
 
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+let links = [
+      {
+        "id": "0",
+        "url": "www.howtographql.com",
+        "description": "Fullstack tutorial for GraphQL"
+      },
+      {
+        "id": "3",
+        "url": "www.prisma.io",
+        "description": "Prisma turns your database into a GraphQL API"
+      },
+      {
+        "id": "4",
+        "url": "www.luke.io",
+        "description": "writing GraphQL API"
+      },
+      {
+        "id": "5",
+        "url": "www.hca.org",
+        "description": "where health is"
+      }
+    ]
 
-const typeDefs = `
-type Query {
-	info: String!
-	feed: [Link!]!
-}
-
-type Link {
-	id: ID!
-	description: String!
-	url: String!
-}
-`
-
-
+let idCount = links.length;
 const resolvers = {
 	Query: {
 		info: () => `This is a GraphQL API`,
 		feed: () => links,
 	},
+  Mutation: {
+    post: (root, { description, url }) => {
+      const nulink = {
+        id: idCount++,
+        description,
+        url,
+      }
+      links.push(nulink)
+      return nulink
+    },
+    updateLink: (root, { id, description, url }) => {
+      const foundID = links.findIndex(i => i.id == id);
+      links[foundID] = { id, description, url };
+      return links[foundID]
+    },
+    deleteLink: (root, { id }) => {
+      const foundID = links.findIndex(i => i.id == id);
+      const link = links[foundID];
+      delete links[foundID];
+      return link;
+    }
+  },
 	Link: {
 		id: (root) => root.id,
     description: (root) => root.description,
@@ -41,7 +67,7 @@ const resolvers = {
 
 
 const server = new GraphQLServer({
-	typeDefs,
+	typeDefs: './src/schema.graphql',
 	resolvers,
 })
 
